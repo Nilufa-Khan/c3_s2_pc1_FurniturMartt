db63bf0bb7c6c31c9cf646a678aae25b4360e97e
Nilufa Khan <khannilufa5@gmail.com>
2022-08-11 14:46:19 +0530

    output statement change

--- src/FurnitureItemIml.java
+++ src/FurnitureItemIml.java
@@ -1,15 +1,20 @@
+import java.util.Scanner;
 public class FurnitureItemIml {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         FurnitureItem obj = new FurnitureItem();
-        obj.furnitureCode = 123;
+        obj.furnitureCode = sc.nextInt();
         System.out.println("Furniture code = "+obj.furnitureCode);
-        obj.furnitureType = "Tables";
+        obj.furnitureType = sc.next();
         System.out.println("Furniture type = "+ obj.furnitureType);
-        obj.gradeOfFurniture = 2;
+        obj.gradeOfFurniture = sc.nextInt();
         System.out.println("Furniture grade = "+ obj.gradeOfFurniture);
-        obj.furnitureUsages = "Outdoor";
-        System.out.println("Furniture usages = "+obj.furnitureUsages);
-        obj.price = 1000.00;
+        obj.furnitureUsages = sc.next();
+        System.out.println("Usages of furniture ="+obj.furnitureUsages);
+        obj.color = sc.next();
+        System.out.println("Color = "+obj.color);
+
+        obj.price = sc.nextDouble();
         System.out.println("Furniture price = "+ obj.price);
         System.out.println("After discount = "+obj.calculation(5));
     }
