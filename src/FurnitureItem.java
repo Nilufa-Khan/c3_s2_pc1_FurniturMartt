7e50a185fc60ea615b28f9820d3d4f69f472486b
Nilufa Khan <khannilufa5@gmail.com>
2022-08-11 13:29:32 +0530

    Code implement

--- src/FurnitureItem.java
+++ src/FurnitureItem.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class FurnitureItem {
+public class FurnitureItem {
+    int furnitureCode;
+    String furnitureType;
+    int gradeOfFurniture;
+    String color;
+    String furnitureUsages;
+    double price;
+    FurnitureItem(){
+    furnitureCode = 0;
+    furnitureType = "";
+    gradeOfFurniture = 0;
+    color = "";
+    furnitureUsages = "";
+    price = 0.0;
+
+    }
+    public double calculation(double discount){
+        double result = 0.0;
+        if(furnitureUsages == "Outdoor"){
+            result = price - ((price * discount) / 100);
+        }
+        return  result;
+    }
+
 }
--- src/FurnitureItemIml.java
+++ src/FurnitureItemIml.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class FurnitureItemIml {
+public class FurnitureItemIml {
+    public static void main(String[] args) {
+        FurnitureItem obj = new FurnitureItem();
+        obj.furnitureCode = 123;
+        System.out.println("Furniture code = "+obj.furnitureCode);
+        obj.furnitureType = "Tables";
+        System.out.println("Furniture type = "+ obj.furnitureType);
+        obj.gradeOfFurniture = 2;
+        System.out.println("Furniture grade = "+ obj.gradeOfFurniture);
+        obj.furnitureUsages = "Outdoor";
+        System.out.println("Furniture usages = "+obj.furnitureUsages);
+        obj.price = 1000.00;
+        System.out.println("Furniture price = "+ obj.price);
+        System.out.println("After discount = "+obj.calculation(5));
+    }
 }
